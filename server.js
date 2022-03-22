16de25578e80f551eca9c88f41eb3a96f93067f5
Mateusz19021996 <mateuszkaleta01@gmail.com>
2022-03-22 07:27:47 +0100

    h5

--- server.js
+++ server.js
@@ -13,4 +13,4 @@ app.get("/*", (req, res) => {
 
 
 
-app.listen(process.env.PORT || 5075, () => console.log("Server działa..."));
+//app.listen(process.env.PORT || 5075, () => console.log("Server działa..."));
