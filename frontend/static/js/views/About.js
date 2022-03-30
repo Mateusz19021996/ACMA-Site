7b97516959696efccbfd751cada9696ce49e5b39
Mateusz19021996 <mateuszkaleta01@gmail.com>
2022-03-30 19:40:07 +0200

    - check behaviour

--- frontend/static/js/views/About.js
+++ frontend/static/js/views/About.js
@@ -1,10 +1,10 @@
 import abstractView from "./AbstractView.js";
 
 export default class extends abstractView {
-
-    constructor(){
-        super();
-        this.setTitle("Page name is ABOUT");
-        this.setPath("/static/html/about.html")        
-    }    
+  constructor() {
+    super();
+    this.setTitle("Page name is ABOUT");
+    this.setPath("/static/html/about.html");
+  }
 }
+//xd
